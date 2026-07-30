2df40c2b55fad694ef0092c95e4f8b769a0f8fa1
l-Automatic <npih33@gmail.com>
2026-07-30 22:58:33 +0200

    export integrerRegularisations

--- packages/calcul-module7/src/index.ts
+++ packages/calcul-module7/src/index.ts
@@ -1,8 +1,10 @@
 export {
   calculerTva,
+  integrerRegularisations,
   type ConfigCalculTva,
   type ResultatCalculTva,
   type LigneCalculTva,
   type CategorieLigneCalcul,
   type EcritureExclue,
+  type RegularisationEncaissement,
 } from './calcul.js';
