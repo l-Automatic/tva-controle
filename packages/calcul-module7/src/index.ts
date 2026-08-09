b8b8bfccd373c0783f0527029aa5060efa489234
l-Automatic <npih33@gmail.com>
2026-08-09 13:47:49 +0200

    export corrigerDeductibleParSoldeFournisseurService

--- packages/calcul-module7/src/index.ts
+++ packages/calcul-module7/src/index.ts
@@ -1,10 +1,12 @@
 export {
   calculerTva,
   integrerRegularisations,
+  corrigerDeductibleParSoldeFournisseurService,
   type ConfigCalculTva,
   type ResultatCalculTva,
   type LigneCalculTva,
   type CategorieLigneCalcul,
   type EcritureExclue,
   type RegularisationEncaissement,
+  type SoldeFournisseurService,
 } from './calcul.js';
