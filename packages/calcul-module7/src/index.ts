8c014c15a9e3a293030b9980af23bbe119400205
l-Automatic <npih33@gmail.com>
2026-08-09 14:19:49 +0200

    revert: retire export corrigerDeductibleParSoldeFournisseurService

--- packages/calcul-module7/src/index.ts
+++ packages/calcul-module7/src/index.ts
@@ -1,12 +1,10 @@
 export {
   calculerTva,
   integrerRegularisations,
-  corrigerDeductibleParSoldeFournisseurService,
   type ConfigCalculTva,
   type ResultatCalculTva,
   type LigneCalculTva,
   type CategorieLigneCalcul,
   type EcritureExclue,
   type RegularisationEncaissement,
-  type SoldeFournisseurService,
 } from './calcul.js';
