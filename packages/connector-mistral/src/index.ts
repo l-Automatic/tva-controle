c9783b463a683f4a7393c94d488d845b2a94f4ab
l-Automatic <npih33@gmail.com>
2026-08-17 13:41:52 +0200

    chore: index connector-mistral

---
+++ packages/connector-mistral/src/index.ts
@@ -0,0 +1,6 @@
+export { MistralClient, MistralApiError, MistralReponseInvalideError, type MistralClientConfig } from './client.js';
+export {
+  suggererClassificationComptes,
+  type CategorieClassification,
+  type SuggestionClassificationCompte,
+} from './classificationComptes.js';
