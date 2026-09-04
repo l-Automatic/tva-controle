243850335daae80d58234b72594fb593a9e6e412
l-Automatic <npih33@gmail.com>
2026-09-04 20:47:06 +0200

    export qualifierEncaissementClientTaux et appliquerCorrectionTauxCollecte

--- packages/orchestrateur-module9/src/index.ts
+++ packages/orchestrateur-module9/src/index.ts
@@ -75,6 +75,8 @@ export {
   qualifierVehiculeTourisme,
   qualifierImmobilisationPotentielle,
   qualifierNouveauTiers,
+  qualifierEncaissementClientTaux,
+  appliquerCorrectionTauxCollecte,
   appliquerTransfertImmobilisation,
   appliquerCorrectionVehiculeTourisme,
   enregistrerRapprochementPaiementAchat,
