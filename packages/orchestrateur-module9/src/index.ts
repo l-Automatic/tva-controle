8dbae506903420b88918065338fe2919de3feeb7
l-Automatic <npih33@gmail.com>
2026-09-08 17:25:12 +0200

    export chargerPortesObligatoires

--- packages/orchestrateur-module9/src/index.ts
+++ packages/orchestrateur-module9/src/index.ts
@@ -32,6 +32,11 @@ export {
 export { verifierAvoirsLegere, type ParametresVerificationAvoirs } from './verifierAvoirsLegere.js';
 export { verifierParcVehicules, type ParametresVerificationParc } from './verifierParcVehicules.js';
 export {
+  chargerPortesObligatoires,
+  type ParametresPortesObligatoires,
+  type EtatPortesObligatoires,
+} from './portesObligatoires.js';
+export {
   verifierVehiculeTourismeLegere,
   type ParametresVerificationVehiculeTourisme,
 } from './verifierVehiculeTourismeLegere.js';
